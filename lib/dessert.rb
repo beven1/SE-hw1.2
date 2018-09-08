99f2857d5e023f5181f9a9cab0efe27f9a2acd52
brandon <beven@uccs.edu>
2018-09-08 23:08:37 +0000

    more functions added

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,19 +1,77 @@
 class Dessert
   def initialize(name, calories)
     # your code here
+    if name.empty? == true
+        raise ArgumentError, "empty array"
+    else
+        @name = name
+    end
+        
+    if calories < 0
+        raise ArgumentError, "price less than zero"
+    else
+        @calories = calories
+    end
   end
-  def healthy?
-    # your code here
+  
+  def name=(new_name)
+    @name = new_name
   end
-  def delicious?
-    # your code here
+  
+  def name
+    return @name
+  end
+  
+  def calories=(new_calories)
+    @calories = new_calories
   end
 
+  def calories
+    return @calories
+  end
+  
+  def healthy?
+    if self.calories < 200
+        return true
+    else
+        return false
+    end
+  end
+  
+  def delicious?
+    return true
+  end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
     # your code here
+    if flavor.empty? == true
+        raise ArgumentError, "empty array"
+    else
+        @flavor = flavor
+    end
+    
+    @calories = 5
+    
+    @name = "#{flavor} jelly bean"
+    
+  end
+
+  def flavor=(new_flavor)
+    @flavor = new_flavor
+  end
+
+  def flavor
+    return @flavor
   end
   # your code here
+  def delicious?
+    puts self.flavor
+    if self.flavor == "licorice"
+        return false
+    else
+      super
+    end
+  end
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,26 @@
 module FunWithStrings
   def palindrome?
     # your code here
+    if self.downcase == self.reverse.downcase || 
+        self.delete("^a-zA-z").downcase == self.delete("^a-zA-z").reverse.downcase
+        return true
+    end
   end
+  
   def count_words
     # your code here
+    hash = Hash.new(0)
+    word = self.delete("^A-Za-z\s*")
+    word_list = word.split(' ')
+    word_list.each { |word_in_list|
+        hash[word_in_list.downcase] += 1
+    }
+    return hash
   end
+  
   def anagram_groups
     # your code here
+    
   end
 end
 
